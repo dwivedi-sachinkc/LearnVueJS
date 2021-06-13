8994c1ca3a7be7b047f9a93474bcd64015584364
= <dwivedi.sachinkc@gmail.com>
2021-06-13 18:16:46 +0530

    completeform

--- src/components/6form/formC.vue
+++ src/components/6form/formC.vue
@@ -6,6 +6,24 @@
     <label for="">password</label>
     <input type="password" name="" id="" v-model="form.password" />
     <br />
+    <select v-model="form.country">
+      <option>india</option>
+      <option>US</option>
+      <option>Australia</option>
+    </select>
+    <br />
+    <h4>technology</h4>
+    <label for="">java</label>
+    <input type="checkbox" v-model="form.technology" value="java" />
+    <label for="">js</label>
+    <input type="checkbox" v-model="form.technology" value="js" />
+    <br />
+    <h4>gender</h4>
+    <label for="">Male</label>
+    <input type="checkbox" v-model="form.gender" value="M" />
+    <label for="">Female</label>
+    <input type="checkbox" v-model="form.gender" value="F" />
+    <br /><br />
     <button type="button" @click="login">submit</button>
   </form>
   <p>{{ form }}</p>
@@ -20,6 +38,9 @@ export default {
       form: {
         email: "",
         password: "",
+        country: "",
+        technology: [],
+        gender: "",
       },
       login,
     };
